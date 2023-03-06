acfc2ef7c3ce0c00a74f2d6351da129ad0f496ad
Kawsar Ahmed <coder.kawsar@gmail.com>
2023-03-06 21:57:42 +0600

    CRUD implemented

--- controllers/tools.controller.js
+++ controllers/tools.controller.js
@@ -1,37 +1,124 @@
-let tools = [
-  { id: 1, name: "Hammer" },
-  { id: 2, name: "Scrue Driver" },
-  { id: 3, name: "Wrench" },
-];
+const { ObjectId } = require("mongodb");
+const { client } = require("../utils/dbConnect");
 
-module.exports.getAllTools = (req, res) => {
-  const { limit, page } = req.query;
-  res.json(tools.slice(0, limit));
+module.exports.getAllTools = async (req, res, next) => {
+  try {
+    const { limit, page } = req.query;
+    const query = {};
+    const db = await client.db("toolsTestDB");
+    const tools = await db
+      .collection("tools")
+      .find(query)
+      .limit(+limit)
+      .skip(+page * limit)
+      .toArray();
+    res.send({ success: true, data: tools });
+  } catch (error) {
+    next(error);
+  }
 };
 
-module.exports.addATool = (req, res) => {
-  tools.push(req.body);
-  res.send(tools);
+module.exports.addATool = async (req, res, next) => {
+  try {
+    const tool = req.body;
+
+    const db = await client.db("toolsTestDB");
+    const result = await db.collection("tools").insertOne(tool);
+    if (result.insertedId) {
+      res.send({
+        status: true,
+        message: `Tool added with id: ${result.insertedId}`,
+      });
+    } else {
+      res
+        .status(400)
+        .send({ success: false, message: "Something went wrong!" });
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
-module.exports.getToolDetails = (req, res) => {
-  const { id } = req.params;
-  const foundTool = tools.find((tool) => tool.id == id);
-  res.send(foundTool);
+module.exports.getToolDetails = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!ObjectId.isValid(id)) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Not a valid tool id" });
+    }
+
+    const query = { _id: ObjectId(id) };
+    const db = await client.db("toolsTestDB");
+    const foundTool = await db.collection("tools").findOne(query);
+    if (foundTool) {
+      res.status(200).json({ success: true, tool: foundTool });
+    } else {
+      res.status(200).json({
+        success: false,
+        message: `Couldn't find a tool with id: ${id}`,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
-module.exports.updateTool = (req, res) => {
-  const { id } = req.params;
-  const newData = tools.find((tool) => tool.id == id);
-  newData.id = id;
-  newData.name = req.body.name;
+module.exports.updateTool = async (req, res, next) => {
+  try {
+    const { id } = req.params;
 
-  res.send(newData);
+    if (!ObjectId.isValid(id)) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Not a valid tool id" });
+    }
+
+    const query = { _id: ObjectId(id) };
+    const db = await client.db("toolsTestDB");
+    const updatedTool = await db.collection("tools").updateOne(query, {
+      $set: req.body,
+    });
+    if (updatedTool.modifiedCount) {
+      res
+        .status(200)
+        .json({ success: true, message: "Successfully updated the tool" });
+    } else {
+      res.status(200).json({
+        success: false,
+        message: `Couldn't update the tool of id: ${id}`,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
-module.exports.deleteTool = (req, res) => {
-  const { id } = req.params;
-  tools = tools.filter((tool) => tool.id != id);
+module.exports.deleteTool = async (req, res, next) => {
+  try {
+    const { id } = req.params;
 
-  res.send(tools);
+    if (!ObjectId.isValid(id)) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Not a valid tool id" });
+    }
+
+    const query = { _id: ObjectId(id) };
+    const db = await client.db("toolsTestDB");
+    const updatedTool = await db.collection("tools").deleteOne(query);
+    if (updatedTool.deletedCount) {
+      res
+        .status(200)
+        .json({ success: true, message: "Successfully deleted the tool" });
+    } else {
+      res.status(200).json({
+        success: false,
+        message: `Couldn't delete the tool of id: ${id}`,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
 };
--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,11 +1,32 @@
-function dbConnect() {
-  // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.roc0q.mongodb.net/?retryWrites=true&w=majority`;
-  // const client = new MongoClient(uri, {
-  //   useNewUrlParser: true,
-  //   useUnifiedTopology: true,
-  //   serverApi: ServerApiVersion.v1,
-  // });
-  console.log("DB connected");
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.otylb.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
+
+async function connectToDB() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Establish and verify connection
+    // await client.db("toolsTestDB").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } catch (err) {
+    console.error(err);
+  }
+}
+// connectToDB().catch(console.dir);
+
+async function closeDB() {
+  try {
+    await client.close();
+    console.log("Connection to MongoDB Atlas closed");
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-module.exports = dbConnect;
+module.exports = {
+  connectToDB,
+  client,
+  closeDB,
+};
